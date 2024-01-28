cc77105897829d83289604934b1b22fa9065b74d
neox56 <neox56@gmail.com>
2024-01-28 14:33:26 +0400

    update app.blade.php

--- storage/framework/views/697d0059d31b2263b976778313992d5f.php
+++ storage/framework/views/697d0059d31b2263b976778313992d5f.php
@@ -20,7 +20,7 @@
     </header>
     <section class="bg-white dark:bg-gray-900">
         <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
-            <?php echo $__env->make('sections.flash', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+            <?php echo $__env->make('flash::message', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
                 <?php echo $__env->yieldContent('content'); ?>
         </div>
     </section>
