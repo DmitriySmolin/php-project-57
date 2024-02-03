e8a7dbe5fd2bd92b9cc52bcc9237e9433335d47f
neox56 <neox56@gmail.com>
2024-02-03 18:58:33 +0400

    add show errors register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,21 +2,22 @@
     <h2 class="text-center">
         <a href="{{ route('home') }}">{{ config('app.name', 'Менеджер задач') }}</a>
     </h2>
+    <!--Validation Errors-->
+    <x-auth-validation-errors class="mb-4" :errors="$errors" />
     <form method="POST" action="{{ route('register') }}">
         @csrf
-
         <!-- Name -->
         <div>
             <x-input-label for="name" :value="__('views.auth.name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+{{--            <x-input-error :messages="$errors->get('name')" class="mt-2" />--}}
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('views.auth.email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+{{--            <x-input-error :messages="$errors->get('email')" class="mt-2" />--}}
         </div>
 
         <!-- Password -->
@@ -28,7 +29,7 @@
                             name="password"
                             required autocomplete="new-password" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+{{--            <x-input-error :messages="$errors->get('password')" class="mt-2" />--}}
         </div>
 
         <!-- Confirm Password -->
@@ -39,7 +40,7 @@
                             type="password"
                             name="password_confirmation" required autocomplete="new-password" />
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+{{--            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />--}}
         </div>
 
         <div class="flex items-center justify-end mt-4">
