926abbb483d19ebaf828b733272da28fd7cc760e
neox56 <neox56@gmail.com>
2024-02-04 23:00:02 +0400

    add laravelcollective for task_status/create.blade.php

--- storage/framework/views/2cb7b1a6acdceb87ca50221cc7a56f6a.php
+++ storage/framework/views/2cb7b1a6acdceb87ca50221cc7a56f6a.php
@@ -2,21 +2,24 @@
     <div class="grid col-span-full">
         <h1 class="mb-5"><?php echo e(__('views.task_status.create.header')); ?></h1>
 
-        <form method="POST" action="<?php echo e(route('task_statuses.store')); ?>" accept-charset="UTF-8" class="w-50"><input
-                name="_token" type="hidden" value="<?php echo e(csrf_token()); ?>">
+        <?php echo e(Form::open(['route' => 'task_statuses.store', 'method' => 'POST', 'class' => 'w-50'])); ?>
+
             <div class="flex flex-col">
                 <div>
-                    <label for="name"><?php echo e(__('views.task_status.create.label')); ?></label>
+                    <?php echo e(Form::label('name', __('views.task_status.create.label'))); ?>
+
                 </div>
                 <div class="mt-2">
-                    <input class="rounded border-gray-300 w-1/3" name="name" type="text" id="name">
+                    <?php echo e(Form::text('name', null, ['class' => 'rounded border-gray-300 w-1/3'])); ?>
+
                 </div>
                 <div class="mt-2">
-                    <input class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit"
-                           value="<?php echo e(__('views.task_status.create.button')); ?>">
+                    <?php echo e(Form::submit(__('views.task_status.create.button'), ['class' => 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'])); ?>
+
                 </div>
             </div>
-        </form>
+        <?php echo e(Form::close()); ?>
+
     </div>
 <?php $__env->stopSection(); ?>
 
