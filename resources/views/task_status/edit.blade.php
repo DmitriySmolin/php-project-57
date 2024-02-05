9e9859d199a4caff11173f2f6d379f029e6ff200
neox56 <neox56@gmail.com>
2024-02-05 15:45:48 +0400

    add task_status/edit.balde.php

--- resources/views/task_status/edit.blade.php
+++ resources/views/task_status/edit.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
     <div class="grid col-span-full">
-        <h1 class="mb-5">{{ __('views.task_status.create.header') }}</h1>
+        <h1 class="mb-5">{{ __('views.task_status.edit.header') }}</h1>
 
-        {{ Form::open(['route' => 'task_statuses.store', 'method' => 'POST', 'class' => 'w-50']) }}
+        {{ Form::model($taskStatus, ['route' => ['task_statuses.update', $taskStatus], 'method' => 'PATCH', 'class' => 'w-50']) }}
         <div class="flex flex-col">
             <div>
-                {{ Form::label('name', __('views.task_status.create.label')) }}
+                {{ Form::label('name', __('views.task_status.edit.label')) }}
             </div>
             <div class="mt-2">
                 {{ Form::text('name', null, ['class' => 'rounded border-gray-300 w-1/3']) }}
@@ -16,7 +16,7 @@
                 @enderror
             </div>
             <div class="mt-2">
-                {{ Form::submit(__('views.task_status.create.button'), ['class' => 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded']) }}
+                {{ Form::submit(__('views.task_status.edit.button'), ['class' => 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded']) }}
             </div>
         </div>
         {{ Form::close() }}
--- storage/framework/views/2cb7b1a6acdceb87ca50221cc7a56f6a.php
+++ storage/framework/views/2cb7b1a6acdceb87ca50221cc7a56f6a.php
@@ -4,30 +4,30 @@
 
         <?php echo e(Form::open(['route' => 'task_statuses.store', 'method' => 'POST', 'class' => 'w-50'])); ?>
 
-            <div class="flex flex-col">
-                <div>
-                    <?php echo e(Form::label('name', __('views.task_status.create.label'))); ?>
+        <div class="flex flex-col">
+            <div>
+                <?php echo e(Form::label('name', __('views.task_status.create.label'))); ?>
 
-                    <?php $__errorArgs = ['name'];
+            </div>
+            <div class="mt-2">
+                <?php echo e(Form::text('name', null, ['class' => 'rounded border-gray-300 w-1/3'])); ?>
+
+                <?php $__errorArgs = ['name'];
 $__bag = $errors->getBag($__errorArgs[1] ?? 'default');
 if ($__bag->has($__errorArgs[0])) :
 if (isset($message)) { $__messageOriginal = $message; }
 $message = $__bag->first($__errorArgs[0]); ?>
-                    <div class="text-rose-600"><?php echo e($message); ?></div>
-                    <?php unset($message);
+                <div class="text-rose-600"><?php echo e($message); ?></div>
+                <?php unset($message);
 if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
 unset($__errorArgs, $__bag); ?>
-                </div>
-                <div class="mt-2">
-                    <?php echo e(Form::text('name', null, ['class' => 'rounded border-gray-300 w-1/3'])); ?>
-
-                </div>
-                <div class="mt-2">
-                    <?php echo e(Form::submit(__('views.task_status.create.button'), ['class' => 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'])); ?>
-
-                </div>
             </div>
+            <div class="mt-2">
+                <?php echo e(Form::submit(__('views.task_status.create.button'), ['class' => 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'])); ?>
+
+            </div>
+        </div>
         <?php echo e(Form::close()); ?>
 
     </div>
