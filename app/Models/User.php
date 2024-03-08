396ea0fbe9168ad0b9030981d0f272c0a36ec938
neox56 <neox56@gmail.com>
2024-03-08 19:07:02 +0400

    add createdTasks

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function createdTasks()
+    {
+        return $this->hasMany(Task::class, 'created_by_id');
+    }
 }
