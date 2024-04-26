32ad84faeaeb090ef646104d908985e8c87944be
neox56 <neox56@gmail.com>
2024-04-26 21:12:05 +0400

    clear cache

--- storage/framework/views/f7e1d82c9c115d742933be429bd289fc.php
+++
@@ -1,5 +0,0 @@
-<button <?php echo e($attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-blue-500 rounded font-bold text-white hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-700 ml-3'])); ?>>
-    <?php echo e($slot); ?>
-
-</button>
-<?php /**PATH /home/neox56/php-project-57/resources/views/components/primary-button.blade.php ENDPATH**/ ?>
